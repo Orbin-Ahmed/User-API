b4efbd8e196e39476067017a5f3d11e7b8cbbd2b
Orbin-Ahmed <orbin.ahmed.acanto@g.bracu.ac.bd>
2024-04-03 05:19:07 +0400

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send(
+    'This is the main page, find the project details <a href="https://github.com/Orbin-Ahmed/user-API/">here</a>'
+  );
+});
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/items", require("./routes/itemRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
